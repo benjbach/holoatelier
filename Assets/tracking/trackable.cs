77a41ad920443219148484715adb81e354d46db4
Benjamin <benj.bach@googlemail.com>
2017-01-19 17:40:14 -0500

    add vuforia

---
+++ Assets/tracking/trackable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trackable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        // print(">>>"  + transform.position.x +","+  transform.position.y +","+ transform.position.z);
+		
+	}
+}
